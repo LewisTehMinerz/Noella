817e1f5b8bc6194695b2ea46a04b721d51ee4da4
Lewis Crichton <lewistehminerz@outlook.com>
2018-06-23 21:26:25 +0100

    remove imports and fix quotes

--- config_example.py
+++ config_example.py
@@ -2,30 +2,7 @@
 ############# Config File #############
 #######################################
 
-from discord.ext import commands
-from collections import Counter
-from collections import OrderedDict, deque, Counter
-from datetime import datetime as dt
-
-import openweathermapy.core as owm
-import cogs.utils.checks
-import cogs.utils.db
-import cogs.utils.formats
-import time
-import logging
-import aiohttp
-import discord
-import sys
-import asyncio
-import datetime
-import traceback
-import copy
-import unicodedata
-import inspect
-import os
-import json
-
-dev_discord = "https://discord.gg/EVfHKKn"
+dev_discord = 'https://discord.gg/EVfHKKn'
 
 client_id   = 'BOTID' # your bot's client ID
 token = 'TOKEN' # your bot's token
@@ -33,10 +10,10 @@ postgresql = 'postgresql://NAME:PASSWORD@LOCALHOST/DB' # your postgresql info fr
 
 carbon_key = '' # your bot's key on carbon's site
 bots_key = '' # your key on bots.discord.pw
-openweathermap_api = "" # your key for OpenWeatherMap API
-youtube_api = "" # your key for YouTube API
-osu_api = "" # your key for osu! API
-Mashape_API = "" # your key for Mashape API
+openweathermap_api = '' # your key for OpenWeatherMap API
+youtube_api = '' # your key for YouTube API
+osu_api = '' # your key for osu! API
+Mashape_API = '' # your key for Mashape API
 
 embed_color = 13454262 #Default Embed Color
 embed_color_succes = 65280 #Default SuccesEmbed Color
